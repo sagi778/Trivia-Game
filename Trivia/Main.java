c3ebe90721198fd6077e0df79c1c8316983d815e
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2021-12-13 15:43:00 +0200

    Update Main.java

--- Trivia/Main.java
+++ Trivia/Main.java
@@ -6,6 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main extends Application {
 
     @Override
@@ -17,7 +21,13 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
+
+        Scanner file = new Scanner( new File("exam.txt") ); // open txt file for reading
+
         launch(args);
+
+        file.close(); // closing txt file
+
     }
 }
