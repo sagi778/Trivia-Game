61af5a156f0addffca2a0c6de8f91b3f454c57ee
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2021-12-14 09:12:16 +0200

    Update Main.java

--- Trivia/Main.java
+++ Trivia/Main.java
@@ -6,9 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+
 
 public class Main extends Application {
 
@@ -16,18 +14,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("TriviaView.fxml"));
         primaryStage.setTitle("My Trivia Exam");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) throws FileNotFoundException{
-
-        Scanner file = new Scanner( new File("exam.txt") ); // open txt file for reading
+    public static void main(String[] args){
 
         launch(args);
-
-        file.close(); // closing txt file
 
     }
 }
