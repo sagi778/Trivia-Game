ef0ceadf9c3dc2fe7d8abeeb7fd0ba7813ab4ac6
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2021-12-14 09:12:59 +0200

    Update TriviaLogic.java

--- Trivia/TriviaLogic.java
+++ Trivia/TriviaLogic.java
@@ -1,15 +1,56 @@
 package Trivia;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TriviaLogic {
 
     //attributes
-    private TriviaQuestion [] _exam; // contain full exam = array of questions & answers
+    private TriviaQuestion [] _exam; // contain full exam = array of questions
 
-    public void initilizeExam(Scanner file){ //creating new exam from txt file
+    public TriviaLogic(){}
+
+    public void initilizeExam(Scanner file) throws FileNotFoundException { //creating new exam from txt file
+
         String row = file.next();
-    }
+        String txt = getText(file);
+        String q = getQuestion(txt);
 
+
+    }
+    public String getText(Scanner file){
+
+        String str = "*";
+
+        while( file.hasNext() ){
+            str += file.next()+ " ";
+        }
+        return str;
+    }
+    public String getQuestion(String txt){ //return question string
+
+        int strt = getNewQuestionStart(txt);
+        int end = getNewQuestionEnd(txt);
+
+        return txt.substring( strt, end )+"?";
+    }
+    public int getNewQuestionStart(String txt){ //get new question beginning index
+
+        int i=0;
+        while( i < txt.length() && !Character.toString(txt.charAt(i)).equals("*") ){
+            i++;
+        }
+        if( i== txt.length() ){return -1;}
+        else return i;
+    }
+    public int getNewQuestionEnd(String txt){ //get new question end index
+
+        int i=0;
+        while( i < txt.length() && !Character.toString(txt.charAt(i)).equals("?") ){
+            i++;
+        }
+        if( i== txt.length() ){return -1;}
+        else return i;
+    }
 }
