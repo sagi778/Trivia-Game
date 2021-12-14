11bfb5da41948e453566f08b6a9e6c9f8482b13a
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2021-12-14 09:11:45 +0200

    Update Controller.java

--- Trivia/Controller.java
+++ Trivia/Controller.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Controller {
 
     @FXML
@@ -14,15 +19,22 @@ public class Controller {
     public RadioButton b3;
     public RadioButton b4;
 
-    TriviaLogic exam = new TriviaLogic();
-
     public void radioAction(ActionEvent event) {
 
         String b = ((RadioButton)event.getSource()).getText(); // get current operation
 
         System.out.println("Answer "+ b + " Picked");
     }
+    public void beginAction(ActionEvent event) throws IOException {
 
+        System.out.println("Begin Exam");
+        TriviaLogic t = new TriviaLogic();
 
+        Scanner file = new Scanner( new File("exam.txt") ); // open txt file for reading
+        t.initilizeExam(file);
 
+    }
+    public void NextAction(ActionEvent actionEvent) {
+        System.out.println("Next");
+    }
 }
