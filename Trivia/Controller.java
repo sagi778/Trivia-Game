0080f40b8fbd562ee7457b3e50817477db185673
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2021-12-16 15:46:42 +0200

    Update Controller.java

--- Trivia/Controller.java
+++ Trivia/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,6 +14,7 @@ import java.util.Scanner;
 public class Controller{
 
     @FXML
+    public Label Title;
     public Label qText;
     public RadioButton b1;
     public RadioButton b2;
@@ -21,36 +23,50 @@ public class Controller{
 
     // construct game logic/model
     TriviaLogic model = new TriviaLogic();
-    private int _qCount = 0;
+    private int _qCount = 0; //number of question index (=question in test)
 
     public void radioAction(ActionEvent event) {
 
-        String b = ((RadioButton)event.getSource()).getText(); // get current operation
+        //String b = ((RadioButton)event.getSource()).getText(); // get current operation
 
-        System.out.println("Answer "+ b + " Picked");
+        // set all rb as 1 group
+        ToggleGroup rbGroup = new ToggleGroup();
+        b1.setToggleGroup(rbGroup);
+        b2.setToggleGroup(rbGroup);
+        b3.setToggleGroup(rbGroup);
+        b4.setToggleGroup(rbGroup);
 
+        boolean correctFlag = isCorrect( model.getQuestionPage(_qCount-1).getRightAnswer(), rbGroup);
+        String correctString = "Wrong";
+        if( correctFlag ){correctString = "correct";}
+        System.out.println( correctString + " Answer");
 
     }
     public void NextAction(ActionEvent event)  {
 
-        //model.newPage( model.getFile() ); // show new question/trivia page
-        qText.setText( model.getQuestionPage(_qCount).getQuestion() );
-        _qCount++;
-        //b1.setText( );
-        System.out.println("Next");
+        if(_qCount < model.getCount()) {
+            qText.setText( model.getQuestionPage(_qCount).getQuestion() );
+            b1.setText( model.getQuestionPage(_qCount).getRightAnswer() );
+            b2.setText( model.getQuestionPage(_qCount).get_answer1() );
+            b3.setText( model.getQuestionPage(_qCount).get_answer2() );
+            b4.setText( model.getQuestionPage(_qCount).get_answer3() );
+            _qCount++;
+        }
+        else{
+            qText.setText("Trivia game is Over...see you next time");
+            return;}
 
     }
     public void correctAnswer(ActionEvent event) {
+
+        String chosenAnswer = ((RadioButton)event.getSource()).getText();
         System.out.println("correct");
     }
+    public boolean isCorrect(String currectAnswer, ToggleGroup rbGroup){
 
-    private void setPage(TriviaQuestion p){ // setting new question to present in GUI
+        String chosenAnswer = ((RadioButton)rbGroup.getSelectedToggle()).getText();
+        return ( chosenAnswer == currectAnswer );
 
-        qText.setText( p.getQuestion() );
-        b1.setText( p.getRightAnswer() );
-        b2.setText( p.get_answer1() );
-        b3.setText( p.get_answer2() );
-        b4.setText( p.get_answer3() );
     }
 
 }
