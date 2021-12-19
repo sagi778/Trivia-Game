1a50f38f76faf34329280e3126b21a0a4ff962ec
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2021-12-19 22:45:25 +0200

    Update Controller.java

--- Trivia/Controller.java
+++ Trivia/Controller.java
@@ -6,11 +6,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Controller{
 
     @FXML
@@ -23,35 +18,35 @@ public class Controller{
 
     // construct game logic/model
     TriviaLogic model = new TriviaLogic();
+    ToggleGroup rbGroup = new ToggleGroup();
     private int _qCount = 0; //number of question index (=question in test)
 
     public void radioAction(ActionEvent event) {
 
-        //String b = ((RadioButton)event.getSource()).getText(); // get current operation
-
         // set all rb as 1 group
-        ToggleGroup rbGroup = new ToggleGroup();
         b1.setToggleGroup(rbGroup);
         b2.setToggleGroup(rbGroup);
         b3.setToggleGroup(rbGroup);
         b4.setToggleGroup(rbGroup);
-
-        boolean correctFlag = isCorrect( model.getQuestionPage(_qCount-1).getRightAnswer(), rbGroup);
-        String correctString = "Wrong";
-        if( correctFlag ){correctString = "correct";}
-        Title.setText( correctString + " Answer" );
-        System.out.println( correctString + " Answer");
-
     }
     public void NextAction(ActionEvent event)  {
 
+        RadioButton bFirst = getRandomRadioButton();
+        RadioButton bSecond = getRandomRadioButton(bFirst);
+        RadioButton bThird = getRandomRadioButton(bSecond,bFirst);
+        RadioButton bFourth = getRandomRadioButton(bThird,bSecond,bFirst);
+
         if(_qCount < model.getCount()) {
+            Title.setText("Question "+ (_qCount+1));
             qText.setText( model.getQuestionPage(_qCount).getQuestion() );
-            b1.setText( model.getQuestionPage(_qCount).getRightAnswer() );
-            b2.setText( model.getQuestionPage(_qCount).get_answer1() );
-            b3.setText( model.getQuestionPage(_qCount).get_answer2() );
-            b4.setText( model.getQuestionPage(_qCount).get_answer3() );
+            bFirst.setText( model.getQuestionPage(_qCount).getRightAnswer() );
+            bSecond.setText( model.getQuestionPage(_qCount).get_answer1() );
+            bThird.setText( model.getQuestionPage(_qCount).get_answer2() );
+            bFourth.setText( model.getQuestionPage(_qCount).get_answer3() );
             _qCount++;
+
+            //set default answer
+            b1.setSelected(true);
         }
         else{
             qText.setText("Trivia game is Over...see you next time");
@@ -60,8 +55,14 @@ public class Controller{
     }
     public void correctAnswer(ActionEvent event) {
 
-        String chosenAnswer = ((RadioButton)event.getSource()).getText();
-        System.out.println("correct");
+        boolean correctFlag = isCorrect( model.getQuestionPage(_qCount-1).getRightAnswer(), rbGroup);
+
+        if(correctFlag){
+            Title.setText("Correct!");
+        }
+        else{
+            Title.setText("Wrong!");
+        }
     }
     public boolean isCorrect(String currectAnswer, ToggleGroup rbGroup){
 
@@ -70,4 +71,60 @@ public class Controller{
 
     }
 
+    // randomization related methods
+    private RadioButton getRandomRadioButton(){
+
+        int MIN = 1, MAX = 4, RANGE = MAX-MIN+1;
+        int rand = (int)(Math.random()*RANGE) + MIN;
+
+        return convertNumToRb(rand);
+
+    } // get random 1-4 radio button to set answer
+    private RadioButton getRandomRadioButton(RadioButton b){// get random 1-4 radio button to set answer that is not set already
+
+        int MIN = 1, MAX = 4, RANGE = MAX-MIN+1;
+        int rand = (int)(Math.random()*RANGE) + MIN;
+        RadioButton rb = convertNumToRb(rand);
+
+        while( rb == b ){
+            rb = getRandomRadioButton();
+        }
+        return rb;
+    }
+    private RadioButton getRandomRadioButton(RadioButton b,RadioButton c){// get random 1-4 radio button to set answer that is not set already
+
+        int MIN = 1, MAX = 4, RANGE = MAX-MIN+1;
+        int rand = (int)(Math.random()*RANGE) + MIN;
+        RadioButton rb = convertNumToRb(rand);
+
+        while( rb==b || rb==c){
+            rb = getRandomRadioButton();
+        }
+        return rb;
+    }
+    private RadioButton getRandomRadioButton(RadioButton b,RadioButton c,RadioButton d){// get random 1-4 radio button to set answer that is not set already
+
+        int MIN = 1, MAX = 4, RANGE = MAX-MIN+1;
+        int rand = (int)(Math.random()*RANGE) + MIN;
+        RadioButton rb = convertNumToRb(rand);
+
+        while( rb==b || rb==c || rb==d ){
+            rb = getRandomRadioButton();
+        }
+        return rb;
+    }
+    private RadioButton convertNumToRb(int num){
+
+        if( num == 1){return b1;}
+        else if( num == 2){return b2;}
+        else if( num == 3){return b3;}
+        else{return b4;}
+    }
+    private int convertRbToNum(RadioButton rb){
+
+        if( rb == b1){return 1;}
+        else if( rb == b2 ){return 2;}
+        else if( rb == b3 ){return 3;}
+        else{return 4;}
+    }
 }
